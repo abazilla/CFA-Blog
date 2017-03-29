ae188da4ffac0301d6a221a8e08dc08eac9feb00
Patrick Abadilla <patrick.abadilla2@gmail.com>
2017-03-29 15:50:50 +1100

    Added Mailer functionality

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,12 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
-  layout 'mailer'
+  default from: 'patrickabadilla2@gmail.com'
+
+  def send_contact_email(email, message)
+    @email = email
+    @message = message
+    mail(to: 'patrickabadilla2@gmail.com', subject: 'New message from website') do |format|
+      format.html { render 'contact_mailer' }
+      format.text { render 'contact_mailer' }
+    end
+  end
 end
